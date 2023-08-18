995391e0dd63f5d43f6f6dfee24f4daf5ac214ea
wallasce <lwallasce@outlook.com>
2023-08-18 14:13:24 -0300

    Chore: increment installed apps and urls

--- supervisorySite/supervisorySite/urls.py
+++ supervisorySite/supervisorySite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ihm.views import Supervisory
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", Supervisory)
 ]
