b359682dab4daa9092913ad86853dc5d89f40af9
wallasce <lwallasce@outlook.com>
2023-08-22 14:02:03 -0300

    Test: Change the serverTest to simulate value of all variables

--- serverTest.py
+++ serverTest.py
@@ -8,12 +8,22 @@ async def main():
     await OPCServerUA.startServer()
     await OPCServerUA.createVariables()
     temperature = 0
+    mode = 'M'
+    setPoint = 5
 
     async with OPCServerUA.server:
         while (temperature < 100):
             await asyncio.sleep(1)
+
+            if (temperature % 10 == 0):
+                mode = 'A' if mode == 'M' else 'M'
+                setPoint += 5
+
             print(temperature)
             temperature += 1
-            await OPCServerUA.setTemperature(temperature)
+            await OPCServerUA.setSetPoint(float(setPoint))
+            await OPCServerUA.setVoltage(float(temperature*0.1))
+            await OPCServerUA.setMode(mode)
+            await OPCServerUA.setTemperature(float(temperature))
 
 asyncio.run(main())
