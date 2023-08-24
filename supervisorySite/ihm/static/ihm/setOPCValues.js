ee6ad75b671edfae5afd453ae74aca79ac7af372
wallasce <lwallasce@outlook.com>
2023-08-24 11:44:38 -0300

    Refactor: Make events to input form generic

--- supervisorySite/ihm/static/ihm/setOPCValues.js
+++ supervisorySite/ihm/static/ihm/setOPCValues.js
@@ -1,15 +1,21 @@
 var csrftoken = document.getElementsByName('csrfmiddlewaretoken')[0].value
 
-document.getElementById('setPoint').onblur = function() {
-    var xhhtp = new XMLHttpRequest();
-    xhhtp.open("POST", "ajax/setOPCSetPoint", true);
-    xhhtp.setRequestHeader("X-CSRFToken", csrftoken); 
-    xhhtp.send(String(this.value));
-}
+function addEventsToInput(id, ajaxFunction) {
+    var inputElement = document.getElementById(id);
+    
+    inputElement.onblur = function() {
+        var xhhtp = new XMLHttpRequest();
+        xhhtp.open("POST", ajaxFunction, true);
+        xhhtp.setRequestHeader("X-CSRFToken", csrftoken); 
+        xhhtp.send(String(this.value));
+    }
 
-document.getElementById('setPoint').onkeyup = function(event) {
-    var keyCode = event.code || event.key;
-    if (keyCode == 'Enter'){
-        this.blur()
+    inputElement.onkeyup = function(event) {
+        var keyCode = event.code || event.key;
+        if (keyCode == 'Enter'){
+            this.blur();
+        }
     }
 }
+
+addEventsToInput('setPoint', 'ajax/setOPCSetPoint');
