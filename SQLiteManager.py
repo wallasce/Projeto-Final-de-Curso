e5ee8d655633e69af69d27f5f60ddbd4e3963b30
wallasce <lwallasce@outlook.com>
2023-08-29 13:31:14 -0300

    Feat: Create a SQLManager

---
+++ SQLiteManager.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+class SQLiteManager:
+    def __init__(self, database : str) -> None:
+        self.connection = sqlite3.connect(database)
+        self.cursor = self.connection.cursor()
+
+    def stopConnection(self) -> None:
+        self.cursor.close()
+
+    def selectAllFrom(self, table : str) -> dict:
+        self.cursor.execute('''
+            SELECT * FROM "''' + table + '''";
+        ''')
+
+        return self.cursor.fetchall()
