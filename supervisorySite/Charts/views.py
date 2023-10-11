0b4d64318b056709c62525b278e4f3b70cdab83b
wallasce <lwallasce@outlook.com>
2023-10-11 15:25:21 -0300

    Feat: Install ChartJS and create a static chart

---
+++ supervisorySite/Charts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from chartjs.views.lines import BaseLineChartView
+
+# Create your views here.
+
+class LineChartJSONView(BaseLineChartView):
+    def get_labels(self):
+        """Return 7 labels for the x-axis."""
+        return ["1", "2", "3", "4", "5", "6", "7"]
+
+    def get_providers(self):
+        """Return names of datasets."""
+        return ["SetPoint", "Temperature"]
+
+    def get_data(self):
+        """Return 2 datasets to plot."""
+
+        return [[20, 20, 20, 20, 20, 20, 20],
+                [10, 15, 16, 17, 18, 19, 20]]
+
+
+line_chart = TemplateView.as_view(template_name='Charts/line_chart.html')
+line_chart_json = LineChartJSONView.as_view()
--- supervisorySite/supervisorySite/settings.py
+++ supervisorySite/supervisorySite/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "ihm",
     "Controller",
+    "chartjs",
+    "Charts",
 ]
 
 MIDDLEWARE = [
