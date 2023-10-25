3c7a9f75aaae602f5ee830ea73a4efe7d6220124
wallasce <lwallasce@outlook.com>
2023-10-25 16:30:55 -0300

    Feat: Create model to Help Content

--- supervisorySite/ContentPage/models.py
+++ supervisorySite/ContentPage/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class HelpContent(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.CharField(max_length=255)
+    fontAwesome = models.CharField(max_length=50)
