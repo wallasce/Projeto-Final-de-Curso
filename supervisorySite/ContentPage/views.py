75ec5fef19ac75e985782ade2108eebc0348d7ad
wallasce <lwallasce@outlook.com>
2023-10-27 01:08:49 -0300

    Feat: Create home page

--- supervisorySite/ContentPage/views.py
+++ supervisorySite/ContentPage/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from .models import HelpContent
+from .models import HelpContent, HomeCard, HomeCardExtra
 
 # Create your views here.
 def homePage(request):
-    return render(request, "ContentPage/home.html")
+    homeCard = HomeCard.objects.all()
+    extraCard = HomeCardExtra.objects.all()
+    
+    parameters = {
+        "cards" : homeCard,
+        "extraCards" : extraCard
+    }
+    return render(request, "ContentPage/home.html", parameters)
 
 def helpPage(request):
     helpContent = HelpContent.objects.all()
