b2ce7b8700a53d2b405cda3e6c0af7e962f378ba
wallasce <lwallasce@outlook.com>
2023-10-26 22:26:07 -0300

    Fear: Create model Extra Card Home

--- supervisorySite/ContentPage/admin.py
+++ supervisorySite/ContentPage/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import HelpContent, HomeCard
+from .models import HelpContent, HomeCard, HomeCardExtra
 # Register your models here.
 admin.site.register(HelpContent)
 admin.site.register(HomeCard)
+admin.site.register(HomeCardExtra)
--- supervisorySite/ContentPage/models.py
+++ supervisorySite/ContentPage/models.py
@@ -10,3 +10,8 @@ class HomeCard(models.Model):
     title = models.CharField(max_length=50)
     body = models.CharField(max_length=255)
     fontAwesome = models.CharField(max_length=50)
+
+class HomeCardExtra(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.CharField(max_length=255)
+    fontAwesome = models.CharField(max_length=50)
